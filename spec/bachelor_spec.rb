a5bb4c1380440efb952857bb840d650ef472abbc
Kolton Starr <starrkolton@gmail.com>
2019-05-17 09:49:24 +0000

    Done.

--- spec/bachelor_spec.rb
+++ spec/bachelor_spec.rb
@@ -2,19 +2,7 @@ describe "bachelor" do
 
   let(:data) { JSON.parse(File.read('spec/fixtures/contestants.json')) }
 
-  describe "#get_first_name_of_season_winner" do
-    it "returns 'Tessa' when passed the data and 'season 10'" do
-      expect(get_first_name_of_season_winner(data, "season 10")).to eq("Tessa")
-    end
-
-    it "returns 'Courtney' when passed the data and 'season 16'" do
-      expect(get_first_name_of_season_winner(data, "season 16")).to eq("Courtney")
-    end
-
-    it "returns 'Emily' when passed the data and 'season 15'" do
-      expect(get_first_name_of_season_winner(data, "season 15")).to eq("Emily")
-    end
-  end
+  
 
   describe "#get_contestant_name" do
     it "returns 'Carly Waddel' when passed data and the string 'Cruise Ship Singer'" do
